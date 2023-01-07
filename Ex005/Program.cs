284900380cf46debaf16564db9213f8bd1ac60bd
vkand <vvkand@mail.ru>
2023-01-07 23:18:10 +0700

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же, Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
